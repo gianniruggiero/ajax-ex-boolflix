c884f5acaea4df37a961158513e3eec3b64b6f61
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-22 13:03:11 +0200

    M1: stampa a video info dei film cercati con la search-bar dall'utente

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  // CLICK sul pulsante Cerca
+  $(".btn_search").click(function(){
+    var searchMovie = $(".input_search").val();
+    console.log(searchMovie);
+    // chiamata Ajax
+    $.ajax({
+      "url":"https://api.themoviedb.org/3/search/movie",
+      "method": "GET",
+      // passa alla chiamata Ajax le chiavi e la Query
+      "data": {
+        "api_key": "a550c2dd96e81e76d012966315a202be",
+        "query": searchMovie,
+        "language": "it-IT",
+      },
+      "success": function(data) {
+        // chiama la funzione per stampare a video i risultati della ricerca
+        renderMovie(data.results);
+      },
+      "error": function(err) {
+        alert ("ATTENZIONE: errore chiamata ajax!");
+      }
+    });
+
+  });
+
+  // funzione che stampa a video i risultati della ricerca che viene passata in argomento
+  function renderMovie(movies) {
+    // ripulisce la lista già stampata a video
+    $("#list-movies li").remove();
+
+    // seleziona per il template da utilizzare
+    var source = $("#movie-template").html();
+    // compila il template selezionato con Handlebars
+    var template = Handlebars.compile(source);
+
+    // stampa il dettaglio di ogni film (oggetto)
+    for (var i = 0; i < movies.length; i++) {
+      var title = movies[i].title;
+      var titleOriginal = movies [i].original_title;
+      var lang = movies [i].original_language;
+      var vote = movies [i].vote_average;
+      // manipola il contenuto delle chiavi dell'oggetto
+      var context = {
+        "title": title,
+        "title_original": titleOriginal,
+        "lang": lang,
+        "vote": vote
+      }
+      // prepara il codice HTML da iniettare
+      var html = template (context);
+      // inserisce il codice html manipolato nel DOM
+      $("#list-movies").append(html);
+    }
+  }
+
+
+
+});
