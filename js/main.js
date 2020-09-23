149edff79e8589cab294c6e09f0602240bb9f040
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-23 12:15:52 +0200

    M2: impostazione voto con grafica stelle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// MILESTONE 2
+
+// 1. Trasformare il voto da 1 a 10 decimale in un numero intero da 1 a 5,
+// poi stampare a schermo un numero di stelle piene che va da 1 a 5, lasciando le restanti vuote
+
+// 2. Trasformare la stringa della lingua in bandiera della nazione corrispondente,
+// gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API
+
+// 3. Allargare la ricerca anche alle serie tv.
+// Con la stessa azione di ricerca prendere sia i film che corrispondono alla query, sia le serie tv
+
+
 $(document).ready(function() {
 
   // CLICK event sul pulsante Cerca
@@ -46,6 +58,7 @@ $(document).ready(function() {
     });
   };
 
+
   // funzione che stampa a video il contentuo dell'oggetto movies passato in argomento
   function printMovie(movies) {
     // cancella, eliminando i <li> della lista già stampata a video
@@ -56,13 +69,53 @@ $(document).ready(function() {
     var template = Handlebars.compile(source);
     // stampa il dettaglio di ogni film presente nell'oggetto {movies}
     for (var i = 0; i < movies.length; i++) {
+      var voteTitle = Math.ceil(movies[i].vote_average / 2);
+      var voteTitleStars = voteInStars(voteTitle)
+      console.log(movies[i].vote_average + " - " + voteTitle);
+
       // manipola il contenuto delle chiavi dell'oggetto con il risultato della chiamta API
-      var context = movies[i];
+      var context = {
+        "title": movies[i].title,
+        "original_title": movies[i].original_title,
+        "original_language": movies[i].original_language,
+        "vote_average": voteTitle + " / " + voteTitleStars,
+      };
       // prepara il codice HTML da iniettare nel DOM
       var html = template (context);
       // inietta nel DOM il codice html manipolato
       $("#list-movies").append(html);
     }
+  };
+
+  // funzione che riceve in argomento un numero intero da 0 a 5 e
+  // ritorna una stringa con corripondenti stelle in fontawesome
+
+  function voteInStars(vote) {
+    var star = "<i class='fas fa-star'></i>";
+    var noStar = "<i class='far fa-star'></i>";
+
+    switch (vote) {
+      case 0:
+        return noStar + noStar + noStar + noStar + noStar;
+        break;
+      case 1:
+        return star + noStar + noStar + noStar + noStar;
+        break;
+      case 2:
+        return star + star + noStar + noStar + noStar;
+        break;
+      case 3:
+        return star + star + star + noStar + noStar;
+        break;
+      case 4:
+        return star + star + star + star + noStar;
+        break;
+      case 5:
+        return star + star + star + star + star;
+    }
   }
+
+
+
 
 });
