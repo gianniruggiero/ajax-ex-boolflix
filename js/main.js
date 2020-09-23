72aeeb627a549f5c1f93ad47811caf68fa68d8f6
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-23 16:39:03 +0200

    M2: ricerca sia film che serie tv con la stringa da input search

--- js/main.js
+++ js/main.js
@@ -12,12 +12,33 @@
 
 $(document).ready(function() {
 
+  var totalTitles = 0;
+
   // CLICK event sul pulsante Cerca
   $(".btn_search").click(function(){
     // controlla che l'inpunt di ricerca non sia vuota
     if (!$(".input_search").val() =="" ) {
       // recupera stringa inserita dall'utente nell'input per la ricerca
-      renderMovie($(".input_search").val());
+      var titleToSearch = $(".input_search").val();
+      // cancella la lista della precedente ricerca, svuota il campo input del search
+      resetSearch()
+      // ricerca nel DB il titolo digitato in input,
+      // con la chiamta API specificata in url
+      // stampa con il template passato in argomento
+      // ricerca tra i film (MOVIE)
+      renderTitles(
+        titleToSearch,
+        "https://api.themoviedb.org/3/search/movie",
+        "movie"
+      );
+      // ricerca tra le serie TV (TV)
+      renderTitles(
+        titleToSearch,
+        "https://api.themoviedb.org/3/search/tv",
+        "tv"
+      );
+      // stampa sulla pagina il numero totale di film trovati
+      $("#total_titles").text(totalTitles);
     }
   });
 
@@ -25,32 +46,52 @@ $(document).ready(function() {
   $(".input_search").keyup(function(e){
     // controlla che sia stato digitato enter e l'inpunt di ricerca non sia vuota
     if (e.keyCode == 13 && !$(".input_search").val() =="" ) {
-      // cancella, eliminando i <li> della lista già stampata a video
-      $("#list-movies li").remove();
       // recupera stringa inserita dall'utente nell'input per la ricerca
-      renderMovie($(".input_search").val());
+      var titleToSearch = $(".input_search").val();
+      // cancella la lista della precedente ricerca, svuota il campo input del search
+      resetSearch()
+      // ricerca nel DB il titolo digitato in input,
+      // con la chiamta API specificata in url
+      // stampa con il template passato in argomento
+      // ricerca tra i film (MOVIE)
+      renderTitles(
+        titleToSearch,
+        "https://api.themoviedb.org/3/search/movie",
+        "movie"
+      );
+      // ricerca tra le serie TV (TV)
+      renderTitles(
+        titleToSearch,
+        "https://api.themoviedb.org/3/search/tv",
+        "tv"
+      );
+      // stampa sulla pagina il numero totale di film trovati
+      $("#total_titles").text(totalTitles);
+      totalTitles = 0;
+
     }
   });
 
   // funzione che fa la ricerca dei titoli contenenti la stringa passata in arogmento
   // e stampa a video i risultati della ricerca
-  function renderMovie(searchMovie) {
+  function renderTitles(searchTitle, urlApi, templateType) {
     // chiamata AJAX all'API
     $.ajax({
-      "url":"https://api.themoviedb.org/3/search/movie",
+      "url": urlApi,
       "method": "GET",
       // passa alla chiamata Ajax le chiavi e la Query
       "data": {
         "api_key": "a550c2dd96e81e76d012966315a202be",
-        "query": searchMovie,
+        "query": searchTitle,
         "language": "it-IT",
         "include_adult": false,
       },
       "success": function(data) {
         // chiama la funzione per stampare a video i risultati della ricerca ritornati dalla chiamata ajax
-        printMovie(data.results);
+        printMovie(data.results, templateType);
         // stampa sulla pagina il numero totale di film trovati
-        $("#total_titles").text(data.total_results);
+        totalTitles = totalTitles + data.total_results;
+        // console.log(totalTitles);
         // posiziona in alto la scroll-bar del listato
         $(".wrapper").scrollTop (0);
       },
@@ -62,26 +103,38 @@ $(document).ready(function() {
 
 
   // funzione che stampa a video il contentuo dell'oggetto movies passato in argomento
-  function printMovie(movies) {
-    // cancella la lista della precedente ricerca, svuota inpunt box di ricerca
-    resetSearch();
+  function printMovie(titles, templateToPrint) {
     // seleziona  il template da utilizzare
-    var source = $("#movie-template").html();
+    var source = $("#title-template").html();
     // compila il template selezionato con Handlebars
     var template = Handlebars.compile(source);
     // stampa il dettaglio di ogni film presente nell'oggetto {movies}
-    for (var i = 0; i < movies.length; i++) {
-      var voteTitle = Math.ceil(movies[i].vote_average / 2);
+    for (var i = 0; i < titles.length; i++) {
+      var voteTitle = Math.ceil(titles[i].vote_average / 2);
       var voteTitleStars = voteInStars(voteTitle)
-      var originalLang = movies[i].original_language;
-      var langFlag = langInFlag(movies[i].original_language);
+      var originalLang = titles[i].original_language;
+      var langFlag = langInFlag(titles[i].original_language);
       if (!langFlag == "") {
         originalLang = "";
+      };
+
+      // definisce che chiavi utilizzare di title[] per riempire il template
+      switch (templateToPrint) {
+        case "movie":
+          var tempTitle = titles[i].title + " (film)";
+          var tempOriginalTitle = titles[i].orignal_title;
+          break;
+        case "tv":
+        var tempTitle = titles[i].name  + " (serie Tv)";
+        var tempOriginalTitle = titles[i].orignal_name;
+        break;
       }
+
+
       // manipola il contenuto delle chiavi dell'oggetto con il risultato della chiamta API
       var context = {
-        "title": movies[i].title,
-        "original_title": movies[i].original_title,
+        "title": tempTitle,
+        "original_title": tempOriginalTitle,
         "original_language": originalLang,
         "vote_average": voteTitleStars,
         "lang_flag": langFlag,
@@ -162,7 +215,7 @@ $(document).ready(function() {
     // cancella, eliminando i <li> della lista già stampata a video
     $("#list-movies li").remove();
     // cancella il testo nell'input di ricerca
-    $(".input_search").val(""); 
+    $(".input_search").val("");
   }
 
 });
