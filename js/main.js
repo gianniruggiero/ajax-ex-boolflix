235ed5c32ff1f87e02d03556a8ce69180991be1c
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-24 12:04:26 +0200

    M2: refactoring funzioni voteInStars() e langInFlag()

--- js/main.js
+++ js/main.js
@@ -12,15 +12,12 @@ $(document).ready(function() {
       var titleToSearch = $(".input_search").val();
       // cancella la lista della precedente ricerca, svuota il campo input del search
       resetSearch()
-      // ricerca nel DB il titolo digitato in input,
-      // con la chiamta API specificata in url
-      // stampa con il template passato in argomento
-      // ricerca tra i film (MOVIE)
+      // (MOVIE) ricerca nel DB il titolo digitato in input, con la chiamta API specificata in url, stampa con il template passato in argomento
       renderTitles(
         titleToSearch,
         "https://api.themoviedb.org/3/search/movie"
       );
-      // ricerca tra le serie TV (TV)
+      // (TV) ricerca nel DB il titolo digitato in input, con la chiamta API specificata in url, stampa con il template passato in argomento
       renderTitles(
         titleToSearch,
         "https://api.themoviedb.org/3/search/tv"
@@ -41,15 +38,12 @@ $(document).ready(function() {
       var titleToSearch = $(".input_search").val();
       // cancella la lista della precedente ricerca, svuota il campo input del search
       resetSearch()
-      // ricerca nel DB il titolo digitato in input,
-      // con la chiamta API specificata in url
-      // stampa con il template passato in argomento
-      // ricerca tra i film (MOVIE)
+      // (MOVIE) ricerca nel DB il titolo digitato in input, con la chiamta API specificata in url, stampa con il template passato in argomento
       renderTitles(
         titleToSearch,
         "https://api.themoviedb.org/3/search/movie"
       );
-      // ricerca tra le serie TV (TV)
+      // (TV) ricerca nel DB il titolo digitato in input, con la chiamta API specificata in url, stampa con il template passato in argomento
       renderTitles(
         titleToSearch,
         "https://api.themoviedb.org/3/search/tv"
@@ -58,7 +52,7 @@ $(document).ready(function() {
   });
 
   // funzione che fa la ricerca dei titoli contenenti la stringa passata in arogmento
-  // e stampa a video i risultati della ricerca
+  // chiamando API endpoint passato in argomento e stampa a video i risultati della ricerca
   function renderTitles(searchTitle, urlApi) {
     // chiamata AJAX all'API
     $.ajax({
@@ -97,13 +91,20 @@ $(document).ready(function() {
     var template = Handlebars.compile(source);
     // stampa il dettaglio di ogni film presente nell'oggetto {movies}
     for (var i = 0; i < titles.length; i++) {
-      var voteTitle = Math.ceil(titles[i].vote_average / 2);
-      var voteTitleStars = voteInStars(voteTitle)
-      var originalLang = titles[i].original_language;
-      var langFlag = langInFlag(titles[i].original_language);
-      if (!langFlag == "") {
-        originalLang = "";
+
+      // manipola il contenuto delle chiavi dell'oggetto con il risultato della chiamta API
+      var context = {
+        "title": titles[i].title,
+        "name": titles[i].name,
+        "original_title": titles[i].original_title,
+        "original_name": titles[i].original_name,
+        "vote_average": voteInStars(titles[i].vote_average),
+        "url_flag": langInFlag(titles[i].original_language),
       };
+      // prepara il codice HTML da iniettare nel DOM
+      var html = template (context);
+      // inietta nel DOM il codice html manipolato
+      $("#list-movies").append(html);
 
       // ................
       // TEST con .onload
@@ -124,7 +125,6 @@ $(document).ready(function() {
       // }
       // ................
 
-
       // ................
       // OPZIONE con switch per gestire chiavi diverse nel template
       // ................
@@ -141,85 +141,39 @@ $(document).ready(function() {
       // }
       // ................
 
-      // manipola il contenuto delle chiavi dell'oggetto con il risultato della chiamta API
-      var context = {
-        "title": titles[i].title,
-        "name": titles[i].name,
-        "original_title": titles[i].original_title,
-        "original_name": titles[i].original_name,
-        "original_language": originalLang,
-        "vote_average": voteTitleStars,
-        "url_flag": langFlag,
-      };
-      // prepara il codice HTML da iniettare nel DOM
-      var html = template (context);
-      // inietta nel DOM il codice html manipolato
-      $("#list-movies").append(html);
     }
   };
 
   // funzione che riceve in argomento un numero intero da 0 a 5 e
   // ritorna una stringa con corripondenti stelle in fontawesome
   function voteInStars(vote) {
-    // assegna le icone con fontawesome
-    // stella piena
-    var star = "<i class='fas fa-star'></i>";
-    // stella vuota
-    var noStar = "<i class='far fa-star'></i>";
-
-    // definisce la stringa da ritornare in base al numero del voto in argomento
-    switch (vote) {
-      case 0:
-        return noStar + noStar + noStar + noStar + noStar;
-        break;
-      case 1:
-        return star + noStar + noStar + noStar + noStar;
-        break;
-      case 2:
-        return star + star + noStar + noStar + noStar;
-        break;
-      case 3:
-        return star + star + star + noStar + noStar;
-        break;
-      case 4:
-        return star + star + star + star + noStar;
-        break;
-      case 5:
-        return star + star + star + star + star;
-    }
+    // trasformiamo il numero in un numero intero da 1 a 5
+    var num = Math.ceil(vote / 2);
+    var string = "";
+    // compone la stringa html
+    for (var i = 0; i <= 4; i++) {
+      if (i <= num) {
+        string += "<i class='fas fa-star'></i>";
+      } else {
+        string += "<i class='far fa-star'></i>";
+      }
+    };
+    return string;
   };
 
   // funzione che riceve in argomento la stringa della lingua e
   // ritorna una stringa con l'indirizzo dell'immagine della bandiera da visualizzare
   function langInFlag(lang) {
-    switch (lang) {
-      case "it":
-        return "img/it.png";
-        break;
-      case "en":
-        return "img/en.png";
-        break;
-      case "fr":
-        return "img/fr.png";;
-        break;
-      case "de":
-        return "img/de.png";
-        break;
-      case "pt":
-        return "img/pt.png";
-        break;
-      case "es":
-        return "img/es.png";
-        break;
-      case "nl":
-        return "img/nl.png";
-        break;
-      case "ja":
-        return "img/ja.png";
-        break;
-      default:
-        return "";
-    }
+    // definisce l'array delle immagini di bandiera presenti nella cartella /img
+    var flags = ["it","en","es","fr","de","nl", "pt", "ja"];
+    // controlla se la lingua passata in argomento è presente nell'array delle bandiere
+    if (flags.includes(lang)) {
+      // ritorna URL per caricare immagine della relativa bandiera
+      return "<img class='ico_flag' src='img/" + lang + ".png' alt=''>"
+    } else {
+      // ritorna la sigla della lingua da stampare a video al posto della bandiera
+      return lang
+    };
   };
 
   // funzione che cancella la lista a video e svuota l'input della ricerca
