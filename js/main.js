2291b5289b68eb0104ceff1b50f642d5f1994380
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-28 03:07:20 +0200

    M5: impostazione chiamata Api per stampare nomi attori

--- js/main.js
+++ js/main.js
@@ -51,14 +51,16 @@ $(document).ready(function() {
         if (data.total_results == 0) {
           if (screen.width > 768) {
             $(".titoli_trovati").addClass("active");
-            // stampa sulla pagina il numero totale di film trovati
+            // stampa sulla pagina avviso che non è stato trovato nessun titolo
             $("#total_titles").text("nessun titolo trovato");
           } else {
-            $(".esito_ricerca").addClass(" active");
+            $(".esito_ricerca").addClass("active");
           }
         } else {
             // chiama la funzione per stampare a video i risultati della ricerca ritornati dalla chiamata ajax
+            console.log(data.results);
             printMovie(data.results, typeTemplate);
+
             // aggiorna variabile del totale dei titoli trovati
             totalTitles = totalTitles + data.total_results;
             // posiziona in alto la scroll-bar del listato
@@ -85,7 +87,7 @@ $(document).ready(function() {
     // stampa il dettaglio di ogni film presente nell'oggetto {movies}
     for (var i = 0; i < titles.length; i++) {
       // definisce le variabili temporanee utilizzate dal ciclo For
-      var tempTitle, tempOriginalTitle, tempSelector, tempTitleType, tempPath, tempNoPoster, tempOverview;
+      var tempTitle, tempOriginalTitle, tempSelector, tempTitleType, tempPath, tempNoPoster, tempOverview, tempActors;
 
       // a seconda del tipo di ricernca, definisce che chiavi utilizzare per riempire i placeholder del template
       switch (templateToPrint) {
@@ -112,13 +114,51 @@ $(document).ready(function() {
         tempPath = "img/no_poster.png";
         tempNoPoster = " active";
       }
-
+      // OVERVIEW
       if (titles[i].overview == ""){
         tempOverview = "";
       } else {
         tempOverview = "<strong>Overview: </strong>" + (titles[i].overview);
-
       }
+
+      // ATTORI
+      $.ajax({
+        "url": "https://api.themoviedb.org/3/" + templateToPrint + "/" + titles[i].id + "/credits",
+        "method": "GET",
+        // passa alla chiamata Ajax le chiavi e la Query
+        "data": {
+          "api_key": "a550c2dd96e81e76d012966315a202be",
+        },
+        "success": function(data) {
+          var listaAttori = "";
+          if (data.cast[0]) {
+            console.log("ARRAY PIENO DI ATTORI");
+            var castLength = 0;
+            if (data.cast.length < 5) {
+              castLength = data.cast.length;
+            } else {
+              castLength = 5;
+            };
+            for (var i = 0; i < castLength; i++) {
+              listaAttori += data.cast[i].name + ", ";
+            }
+            console.log("listaAttori: " + listaAttori);
+            tempActors = "<strong>Cast: </strong>" + listaAttori;
+
+          } else {
+            console.log("ARRAY VUOTO");
+            tempActors = "<strong>Cast: </strong> non disponibile";
+          }
+
+          // console.log(strActors);
+          console.log("*********************");
+          // return strActor;
+          // return listaAttori;
+        },
+        "error": function(err) {
+          alert ("ATTENZIONE: errore chiamata ajax!");
+        }
+      });
 
       // manipola il contenuto delle chiavi dell'oggetto con il risultato della chiamta API
       var context = {
@@ -130,11 +170,15 @@ $(document).ready(function() {
         "url_poster": tempPath,
         "overview": tempOverview,
         "class_noposter": tempNoPoster,
+        "actors": tempActors,
       };
       // prepara il codice HTML da iniettare nel DOM
       var html = template (context);
       // inietta nel DOM il codice html manipolato
       tempSelector.append(html);
+
+      // getActors (titles[i].id, templateToPrint);
+
     }
   };
 
@@ -183,9 +227,10 @@ $(document).ready(function() {
     // cancella la label Serie TV prima del corrispondente listato
     $("#tit_tv").text("");
     // nasconde H3 per avviso Nessun titolo trovato
-    $(".esito_ricerca").removeClass(" active");
+    $(".esito_ricerca").removeClass("active");
   }
 
+  // funziona che accorpa reset e chiamate ajax negli eventi della ricerca
   function titlesSearchPrint() {
     // resetta variabile del numero di titoli caricati, che viene stampato a video dopo la ricerca
     totalTitles = 0;
@@ -198,5 +243,94 @@ $(document).ready(function() {
     // ricerca titolo digitato in input, con endpoint della chiamata API specificata in argomento
     renderTitles(titleToSearch, "tv");
   }
+
+
+  function getActors(id_title, type_title) {
+    $.ajax({
+      "url": "https://api.themoviedb.org/3/" + type_title + "/" + id_title + "/credits",
+      "method": "GET",
+      // passa alla chiamata Ajax le chiavi e la Query
+      "data": {
+        "api_key": "a550c2dd96e81e76d012966315a202be",
+      },
+      "success": function(data) {
+        var listaAttori = "";
+        var strActors = [];
+        if (data.cast[0]) {
+          console.log("ARRAY PIENO DI ATTORI");
+          var castLength = 0;
+          if (data.cast.length < 5) {
+            castLength = data.cast.length;
+          } else {
+            castLength = 5;
+          };
+          for (var i = 0; i < castLength; i++) {
+            strActors[i] = data.cast[i].name
+            listaAttori += data.cast[i].name + ", ";
+          }
+          printActors (strActors, type_title)
+        } else {
+          console.log("ARRAY VUOTO");
+          printActors (strActor, type_title)
+        }
+        // return strActor;
+        // return listaAttori;
+      },
+      "error": function(err) {
+        alert ("ATTENZIONE: errore chiamata ajax!");
+      }
+    });
+
+
+  // funzione che stampa a video i nomi degli attori
+  function printActors(titles, type) {
+
+    // definisce se appenderà alla lista Film oppure serieTV, in base al tipo passato in argomento
+    switch (type) {
+      case "movie":
+        tempSelector = $("#list-movies");
+        break;
+      case "tv":
+        tempSelector = $("#list-tv");
+        break;
+    };
+
+    if (titles[0]) {
+      // l'array contiene nomi attori
+      var arrActors = titles;
+      for (var i = 0; i < arrActors.length; i++) {
+        if (i == arrActors.length - 1) {
+          tempActors += arrActors[i];
+        } else {
+          tempActors += arrActors[i] + ", ";
+        }
+      }
+      var tempActors = "<strong>Cast: </strong>" + tempActors;
+    } else {
+      // l'array NON contiene nomi degli attori
+      var tempActors = "<strong>Cast: </strong>" + "non disponibile";
+    }
+
+    // seleziona  il template da utilizzare
+    var source = $("#title-template").html();
+    // compila il template selezionato con Handlebars
+    var template = Handlebars.compile(source);
+    // manipola il contenuto delle chiavi dell'oggetto con il risultato della chiamta API
+    var context = {
+      "actors": tempActors,
+    };
+    // prepara il codice HTML da iniettare nel DOM
+    var html = template (context);
+    // inietta nel DOM il codice html manipolato
+    tempSelector.append(html);
+    }
+  };
+
+
+
+
+
+
+
 
 });
